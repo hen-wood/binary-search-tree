ae2dfdaf35f18fd58ef22f74283f90ef91691b37
Keenly <keenlyhere@gmail.com>
2022-11-02 12:29:09 -0700

    bst min to bt max

--- tree-practice.js
+++ tree-practice.js
@@ -5,23 +5,86 @@ const { BinarySearchTree, TreeNode } = require('./binary-search-tree.js');
 // Practice problems on binary trees
 
 function findMinBST (rootNode) {
-  // Your code here
+  let curr = rootNode;
+
+  while (curr.left) {
+    curr = curr.left;
+  }
+
+  return curr.val;
 }
 
 function findMaxBST (rootNode) {
-  // Your code here
+  let curr = rootNode;
+
+  while (curr.right) {
+    curr = curr.right;
+  }
+
+  return curr.val;
 }
 
 function findMinBT (rootNode) {
-  // Your code here
+  // const queue = [rootNode];
+  // let min = Infinity;
+
+  // while (queue.length) {
+  //   let node = queue.shift();
+
+  //   if (node.val < min) {
+  //     min = node.val;
+  //   }
+
+  //   if (node.left) {
+  //     queue.push(node.left);
+  //   }
+
+  //   if (node.right) {
+  //     queue.push(node.right);
+  //   }
+  // }
+
+  // return min;
+
+  if (!rootNode) return;
+
+  let min = rootNode.val;
+  let left = findMinBT(rootNode.left);
+  let right = findMinBT(rootNode.right);
+
+  if (left < min) min = left;
+  if (right < min) min = right;
+
+  return min;
 }
 
 function findMaxBT (rootNode) {
-  // Your code here
+
+  if (!rootNode) return;
+
+  let max = rootNode.val;
+  let left = findMaxBT(rootNode.left);
+  let right = findMaxBT(rootNode.right);
+
+  if (left > max) max = left;
+  if (right > max) max = right;
+
+  return max;
 }
 
 function getHeight (rootNode) {
-  // Your code here
+  if (!rootNode) return -1;
+
+  // initialize let left = rootNode.left
+  // same for right = rootNode.right
+  // while either exist, continue reassigning their values
+  // break out of loop when reach end
+  // keep counter to tell height
+  while (rootNode.left || rootNode.right) {
+    if (rootNode.left) {
+      //
+    }
+  }
 }
 
 function balancedTree (rootNode) {
@@ -55,7 +118,7 @@ function deleteNodeBST(rootNode, target) {
 
   // Case 2: Two children:
   //  Set the value to its in-order predecessor, then delete the predecessor
-  //  Replace target node with the left most child on its right side, 
+  //  Replace target node with the left most child on its right side,
   //  or the right most child on its left side.
   //  Then delete the child that it was replaced with.
 
